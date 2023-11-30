eb3fcd8365b2476673f3173d3288022cf9af83b7
Ruslan <ruslan.leonoff2014@yandex.ru>
2023-11-30 11:34:01 +0300

    project 3

---
+++ project3/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 3: Задайте массив из вещественных чисел с ненулевой дробной частью.
+            // Найдите разницу между максимальным и минимальным элементов массива.
+
+            double[] array = {3.4, 6.5, 1.2, 6.7};
+            double maxItem = array.Max(), minItem = array.Min();
+            Console.WriteLine($"Max {maxItem}");
+            Console.WriteLine($"Min {minItem}");
+            Console.WriteLine(Math.Abs(maxItem - minItem));
